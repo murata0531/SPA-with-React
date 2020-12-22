ddce2102b0b2a053a3d70f7a27e31c091a808a77
murata0531 <coless115@gmail.com>
2020-12-22 10:16:36 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import Top from './components/Top';
+import About from './components/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <ul>
+          <li><Link to="/">トップ</Link></li>
+          <li><Link to="/about/">アバウト</Link></li>
+        </ul>
+        <Route path="/" exact component={Top}></Route>
+        <Route path="/about/" exact component={About}></Route>
+    </Router>
   );
 }
 
